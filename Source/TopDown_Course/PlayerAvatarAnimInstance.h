fae301de36689a415fae7375bbbafe8b6f886c49
ON4Saleh <116005799+ON4Saleh@users.noreply.github.com>
2025-04-14 21:11:37 +0200

    animation

---
+++ Source/TopDown_Course/PlayerAvatarAnimInstance.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PlayerAvatarAnimInstance.generated.h"
+UENUM(BlueprintType)
+enum class EPlayerState : uint8
+{
+	Locomotion = 0,
+	Attack,
+	Hit,
+	Die
+};
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWN_COURSE_API UPlayerAvatarAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Speed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool IsAttacking;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EPlayerState State;
+	UFUNCTION(BlueprintCallable)
+	void OnStateAnimationEnds();
+};
